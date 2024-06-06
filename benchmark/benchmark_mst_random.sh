014c6495e8c4fe3d8b63e0fa8ab8aa20cd1a3f79
Magnus Siegumfeldt <m.ks@outlook.dk>
2024-06-06 13:45:09 +0200

    bench compiler script

--- benchmark/benchmark_mst_random.sh
+++ benchmark/benchmark_mst_random.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-DATA_FILE=mst.jsonl
+#DATA_FILE=mst.jsonl
 REPEAT=1
 SETS=3
 WARMUPS=1
@@ -9,18 +7,36 @@ ITERATIONS=3
 
 if [ -f $INPUT_FILE ]
 then
-    mkdir -p results/splay_top_tree/
-    rm -f results/splay_top_tree/$DATA_FILE
+    mkdir -p results/mst/
+    rm -f results/mst/splay-top-tree.jsonl
     
     for n in 1000 2000 5000 10000 20000 50000 100000 200000 500000 1000000
     do
-        echo "Benchmark MST with $n vertices"...
+        echo "Benchmark splay-top-tree MST with $n vertices"...
         for i in $(eval echo {0..$(($SETS - 1))})
         do
             echo "Set $i" 
             for _ in $(eval echo {1..$REPEAT})
             do
-                ./../build/benchmark_mst $WARMUPS $ITERATIONS dataset/mst/random/mst_${n}_${i}.txt >> results/splay_top_tree/$DATA_FILE || exit
+                ./bin/splay-top-trees/benchmark_mst $WARMUPS $ITERATIONS dataset/mst/random/mst_${n}_${i}.txt >> results/mst/splay-top-tree.jsonl || exit
+            done
+        done
+    done
+fi
+if [ -f $INPUT_FILE ]
+then
+    mkdir -p results/mst/
+    rm -f results/mst/stt-rs.jsonl
+    
+    for n in 1000 2000 5000 10000 20000 50000 100000 200000 500000 1000000
+    do
+        echo "Benchmark stt-rs MST with $n vertices"...
+        for i in $(eval echo {0..$(($SETS - 1))})
+        do
+            echo "Set $i" 
+            for _ in $(eval echo {1..$REPEAT})
+            do
+                ./bin/stt-rs -i dataset/mst/random/mst_${n}_${i}.txt --json >> results/mst/stt-rs.jsonl || exit
             done
         done
     done
--- benchmark/build_benchmarks.sh
+++ benchmark/build_benchmarks.sh
@@ -1,9 +1,23 @@
+#Splay top trees
+mkdir -p ../splay-top-trees/build
 cd ../splay-top-trees/build
 cmake -DCMAKE_BUILD_TYPE=Release ..
-cmake --build --target benchmark_mst --config Release
+cmake --build . --target benchmark_mst benchmark_connectivity --config Release
+mkdir -p ../../benchmark/bin/splay-top-trees/
+mv benchmark_mst ../../benchmark/bin/splay-top-trees/benchmark_mst
+mv benchmark_connectivity ../../benchmark/bin/splay-top-trees/benchmark_connectivity
+# stt-c
+cd ../../stt-c
+make
+mkdir -p ../benchmark/bin/stt-c
+mv bin/greedy_stt ../benchmark/bin/stt-c/greedy_stt
 
-
-
-
-cd ../benchmark
+# stt-rs
+cd ../stt-rs/stt-benchmarks
+cargo build --bin bench_queries_file --release
+cargo build --bin bench_mst --release
+mkdir -p ../../benchmark/bin/stt-rs
+mv target/release/bench_queries_file ../../benchmark/bin/stt-rs/bench_queries_file
+mv target/release/bench_mst ../../benchmark/bin/stt-rs/bench_mst
+cd ../../benchmark/
 
