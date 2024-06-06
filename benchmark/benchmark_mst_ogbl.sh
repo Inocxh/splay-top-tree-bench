8dea3f78943e98d5da4268f5207e44690557f658
Magnus Siegumfeldt <m.ks@outlook.dk>
2024-06-06 14:21:34 +0200

    collab runner

--- benchmark/benchmark_mst_ogbl.sh
+++ benchmark/benchmark_mst_ogbl.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-
-INPUT_FILE=dataset/mst/ogbl-collab/incremental_mst.txt
-DATA_FILE=mst_ogbl.jsonl
-REPEAT=5
 WARMUPS=1
 ITERATIONS=3
 
-
 if [ -f $INPUT_FILE ]
 then
-    mkdir -p results/splay_top_tree/
-    rm -f results/splay_top_tree/$DATA_FILE
-    
-    echo "Benchmark MST on ogbl-collab dataset..."
-    for _ in $(eval echo {1..$REPEAT})
-    do
-        ./../build/benchmark_mst $WARMUPS $ITERATIONS $INPUT_FILE >> results/splay_top_tree/$DATA_FILE || exit
-    done
-else
-    echo "Dataset file $INPUT_FILE does not exist"
+    mkdir -p results/mst-ogbl-collab/
+    rm -f results/mst-ogbl-collab/splay-top-tree.jsonl
+    ./bin/splay-top-trees/benchmark_mst $WARMUPS $ITERATIONS dataset/mst/ogbl-collab/incremental_mst.txt >> results/mst-ogbl-collab/splay-top-tree.jsonl || exit
+
+fi
+if [ -f $INPUT_FILE ]
+then
+    mkdir -p results/mst/
+    rm -f results/mst/stt-rs.jsonl
+   
+    ./bin/stt-rs -i dataset/mst/ogbl-collab/incremental_mst.txt --json >> results/mst-ogbl-collab/stt-rs.jsonl || exit
 fi
