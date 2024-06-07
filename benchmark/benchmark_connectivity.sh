475fd983f1f8f0c6b9457ec25009e3ec00dde487
Inocxh <s204453@student.dtu.dk>
2024-06-07 09:42:43 +0200

    made connectivity work with rust

--- benchmark/benchmark_connectivity.sh
+++ benchmark/benchmark_connectivity.sh
@@ -19,18 +19,14 @@ rm -f results/connectivity/stt-c.jsonl
 for n in 500 1000 2000 3000 4000 5000 6000 7000 8000
 do
     echo "Benchmark stt-c connectivity with $n vertices"...
-    ./bin/stt-c/greedy_stt bench 1 $WARMUPS $ITERATIONS dataset/connectivity/con_${n}_0.txt >> results/connectivity/stt-rs.jsonl || exit   
+    ./bin/stt-c/greedy_stt bench 1 $WARMUPS $ITERATIONS dataset/connectivity/con_${n}_0.txt >> results/connectivity/stt-c.jsonl || exit   
 done
+
+mkdir -p results/connectivity/
+rm -f results/connectivity/stt-rs.jsonl
 
 for n in 500 1000 2000 3000 4000 5000 6000 7000 8000
 do
     echo "Benchmark stt-rs trees with $n vertices"...
-    for i in $(eval echo {0..$(($SETS - 1))})
-    do
-        echo "Set $i" 
-        for _ in $(eval echo {1..$REPEAT})
-        do
-            bin/stt-rs/bench_queries_file --input-file dataset/con/con_${n}_0.txt --iterations $ITERATIONS >> results/con/$STT_RS_FILE || exit
-        done
-    done
+    bin/stt-rs/bench_queries_file --input-file dataset/connectivity/con_${n}_0.txt --iterations $ITERATIONS >> results/connectivity/stt-rs.c || exit
 done
