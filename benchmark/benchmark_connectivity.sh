1712fce4bea64a1374c7878ef6a5dedcbae9657e
Magnus Siegumfeldt <m.ks@outlook.dk>
2024-06-07 09:39:18 +0200

    bench work

--- benchmark/benchmark_connectivity.sh
+++ benchmark/benchmark_connectivity.sh
@@ -8,17 +8,18 @@ rm -f results/connectivity/splay-top-tree.jsonl
 
 for n in 500 1000 2000 3000 4000 5000 6000 7000 8000
 do
-    echo "Benchmark splay-top-tree connectivity with $n vertices"...           
-    ./bin/splay-top-trees/benchmark_connectivity $WARMUPS $ITERATIONS dataset/connectivity/con_${n}_${i}.txt >> results/connectivity/splay-top-tree.jsonl || exit   
+   echo "Benchmark splay-top-tree connectivity with $n vertices"...           
+   ./bin/splay-top-trees/benchmark_connectivity $WARMUPS $ITERATIONS dataset/connectivity/con_${n}_0.txt >> results/connectivity/splay-top-tree.jsonl || exit   
 done
 
+
 mkdir -p results/connectivity/
-rm -f results/connectivity/stt-rs.jsonl
+rm -f results/connectivity/stt-c.jsonl
 
 for n in 500 1000 2000 3000 4000 5000 6000 7000 8000
 do
     echo "Benchmark stt-c connectivity with $n vertices"...
-    ./bin/stt-c/greedy_stt bench 1 $WARMUPS $ITERATIONS dataset/connectivity/con_${n}_${i}.txt >> results/connectivity/stt-rs.jsonl || exit   
+    ./bin/stt-c/greedy_stt bench 1 $WARMUPS $ITERATIONS dataset/connectivity/con_${n}_0.txt >> results/connectivity/stt-c.jsonl || exit   
     
 done
 
